33a2b9a593a41e2d4d24a7a5284ca59cfd57b2c3
jerry hung <jerry110160@gmail.com>
2020-06-03 20:38:40 -0400

    add shared user agent

---
+++ lib/shared/user_agent.dart
@@ -0,0 +1,8 @@
+class UserAgent {
+  static Map<String, String> fromType = {
+    'Mobile':
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25',
+    'Desktop':
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'
+  };
+}
